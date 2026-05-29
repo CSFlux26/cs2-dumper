b0c62a1a21e4681a9ba5246fb7d53752972896f5
CSFlux26 <sebodotca@gmail.com>
2026-05-28 20:53:05 -0400

    Update particles_dll.zig

--- particles_dll.zig
+++ particles_dll.zig
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-05-22 21:00:25.760070800 UTC
+// 2026-05-29 00:51:00.511729600 UTC
 
 pub const cs2_dumper = struct {
     pub const schemas = struct {
