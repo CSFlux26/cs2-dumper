b7b56e168e0a2ee8ac66fe35765cdb950cdb0d95
CSFlux26 <sebodotca@gmail.com>
2026-07-20 21:35:17 -0400

    Update server_dll.rs

--- server_dll.rs
+++ server_dll.rs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-07-10 23:05:36.284223 UTC
+// 2026-07-21 01:33:05.984652500 UTC
 
 #![allow(non_upper_case_globals, non_camel_case_types, non_snake_case, unused)]
 
