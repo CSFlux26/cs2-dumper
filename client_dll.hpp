483997ca5f149b0902501b9e6dbdc49164f29210
CSFlux26 <sebodotca@gmail.com>
2026-08-10 23:11:18 -0400

    Update client_dll.hpp

--- client_dll.hpp
+++ client_dll.hpp
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-08-03 22:06:00.674072800 UTC
+// 2026-08-11 03:10:31.465109700 UTC
 
 #pragma once
 
@@ -10,7 +10,7 @@ namespace cs2_dumper {
     namespace schemas {
         // Module: client.dll
         // Class count: 563
-        // Enum count: 14
+        // Enum count: 18
         namespace client_dll {
             // Alignment: 4
             // Member count: 5
