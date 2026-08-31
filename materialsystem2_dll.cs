4b9c83e2efd1fc3acb48bbcb270d4f92fbebeb02
CSFlux26 <sebodotca@gmail.com>
2026-08-31 08:46:58 -0400

    Update materialsystem2_dll.cs

--- materialsystem2_dll.cs
+++ materialsystem2_dll.cs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-08-14 04:03:00.632048800 UTC
+// 2026-08-31 12:40:58.021861100 UTC
 
 namespace CS2Dumper.Schemas {
     // Module: materialsystem2.dll
