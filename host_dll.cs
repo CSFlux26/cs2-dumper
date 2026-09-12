c989f54f9e5dcdfb7c839d185d7a6a48674932fc
CSFlux26 <sebodotca@gmail.com>
2026-09-12 10:39:39 -0400

    Update host_dll.cs

--- host_dll.cs
+++ host_dll.cs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-08-31 12:40:58.021861100 UTC
+// 2026-09-12 14:38:46.740864500 UTC
 
 namespace CS2Dumper.Schemas {
     // Module: host.dll
